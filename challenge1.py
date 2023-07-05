9e73a390f3c9c05b39c5316a7f78009539f08085
Gustavo Lopes <gustaa.lopes@gmail.com>
2023-07-05 17:17:58 -0300

    Adicionando funções para questão 4 e 5

--- challenge1.py
+++ challenge1.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import re
+
 '''
 1) Reverta a ordem das palavras nas frases, mantendo a ordem das palavras:
 '''
@@ -56,3 +59,40 @@ def sub_palindrome(string):
 a = 'babad'
 
 print(sub_palindrome(a))
+
+'''
+4) Coloque em maiúscula a primeira letra de cada frase na string:
+'''
+
+# Usando o módulo regular expressions 'import re'
+def capitalize_first(string):
+    capitalized_sentence = ''
+    li = re.split(r'(?<=[.!?])\s', string) # Delimitando os caracteres de pontuação e criando a lista que vai armazenar cada frase
+
+    capitalized_list = []
+    for i in range(0, len(li)):
+        capitalized_list.append(li[i].capitalize()) # Tornando maiúscula cada frase separada e armazenando numa lista
+
+    capitalized_sentence = ' '.join(capitalized_list) # Juntando as frases da lista criada anteriormente
+    return capitalized_sentence
+
+
+a = "hello. how are you? i'm fine, thank you."
+print(capitalize_first(a))
+
+
+'''
+5) Verifique se a string é um anagrama de um palíndromo:
+'''
+# Utilizando o módulo counter que conta valores e cria um dicionário a partir disso
+
+def anagram_palindrome(string):
+    char_values = Counter(string) #transformando a string em um dicionário com a quantidade de cada letra estabelecida
+    odd_count = 0 # Quantidade de caracteres ímpares na string
+    for odd in char_values.values(): # checa quantidade de cada caractere. Se for impar a contagem sobe +1
+        if odd % 2 != 0:
+            odd_count += 1
+    return odd_count <= 1 # Retorna True para anagrama de um palíndromo se a contagem de caracteres ímpares seja = ou < que 1
+
+string = 'racecar'
+print(anagram_palindrome(string))
